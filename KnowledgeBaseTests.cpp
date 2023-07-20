13b5e9efb01762d852a0e45791884d77ead16608
Binary12 <rickejizu@vt.edu>
2023-07-20 10:40:30 -0400

    first upload

---
+++ KnowledgeBaseTests.cpp
@@ -0,0 +1,24 @@
+//=== ExpressionTreeTests.cpp - Catch tests of tokenize implementation
+// 
+//=================//
+#include "catch.hpp"
+
+#include "Parser.hpp"
+#include "ExpressionTree.hpp"
+#include "Lexer.hpp"
+#include "KnowledgeBase.hpp"
+#include <sstream>
+#include <string>
+
+using namespace vtpl;
+using namespace std;
+
+TEST_CASE("Parse KnowledgeBase")
+{
+	tuple<ParseError, vtpl::KnowledgeBase> knowledgeBase;
+	string input = "h(X,Y) :- f(X),g(b,Y). ";
+	istringstream iss(input);
+	TokenList t1 = tokenize(iss);
+	knowledgeBase = parseKnowledgeBase(t1);
+	REQUIRE(!std::get<0>(knowledgeBase).isSet());
+}
